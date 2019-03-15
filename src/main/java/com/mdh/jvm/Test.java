04dc290b467a9811f269e3d3d556a1c7d6876c81
mdh-lcb <752228071@qq.com>
2019-03-15 10:21:03 +0800

    redis 事务

--- src/main/java/com/mdh/jvm/Test.java
+++ src/main/java/com/mdh/jvm/Test.java
@@ -5,4 +5,8 @@ package com.mdh.jvm;
  * @Date: 2019/3/11 10:12
  */
 public class Test {
+    public static void main(String[] args) {
+        System.out.println(2<<1);
+        System.out.println(6>>1);
+    }
 }
