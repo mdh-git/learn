68b3feaa309780817cb8edc804aa941186e68da6
mdh-lcb <752228071@qq.com>
2019-03-19 18:19:53 +0800

    HashMap

---
+++ src/main/java/com/mdh/datastructure/ConcurrentHashMap/Demo.java
@@ -0,0 +1,13 @@
+package com.mdh.datastructure.ConcurrentHashMap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Author: madonghao
+ * @Date: 2019/3/19 11:31
+ */
+public class Demo {
+
+    Map map = new HashMap();
+}
