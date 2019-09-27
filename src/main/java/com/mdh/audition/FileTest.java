75a8ee33121449f3e99465b1e80f0189224639c4
donghao.ma <donghao.ma@xiaoi.com>
2019-09-27 16:03:43 +0800

    feat: 同事面试题目

--- src/main/java/com/mdh/audition/FileTest.java
+++ src/main/java/com/mdh/audition/FileTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import java.io.File;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.*;
 
 /**
  * DESCRIPTION: 2019-09-24号电话面试的题目
@@ -24,6 +25,18 @@ public class FileTest {
         System.out.println(file.getPath());
         // 获取父路径
         System.out.println(file.getParent());
+        // 获取父路径
+        System.out.println(file.getParentFile());
+        // 测试此抽象路径名表示的文件或目录是否存在
+        System.out.println(file.exists());
+        // 测试此抽象路径名表示的文件是否是一个目录
+        System.out.println(file.isDirectory());
+        // 测试此抽象路径名表示的文件是否是一个标准文件
+        System.out.println(file.isFile());
+        // 返回此抽象路径名表示的文件最后一次被修改的时间
+        System.out.println(file.lastModified());
+        // 返回由此抽象路径名表示的文件的长度
+        System.out.println(file.length());
     }
 
     /**
@@ -88,4 +101,43 @@ public class FileTest {
     }
 
 
+    @Test
+    public void map(){
+        List<Map<String,Object>> resultList= Collections.emptyList();
+        resultList.add(new HashMap<>());
+    }
+
+    /**
+     * 问题：
+     * 小明在一家服装店工作。他有一大堆袜子，但他必须按颜色配对出售。
+     * 给定一组表示每个袜子颜色的整数，确定有多少对具有匹配颜色的袜子。
+     * 例如，有7只带有颜色的袜子，分别是 [1,2,1,2,1,3,2]。有一双带有颜色1的袜子，和一双带有颜色2的袜子，最后剩下三个落单的袜子，所以最后有2双成对的袜子。
+     * 输入格式：
+     * 第一行包含一个整数，表示袜子只数。第二行包含空格分隔的整数，用于描述每只袜子的颜色。
+     * 输出格式：
+     * 返回可以出售的成对袜子的总数。
+     */
+    @Test
+    public void mapTest(){
+        HashMap<String, Integer> map = new HashMap();
+        List<String> list = Arrays.asList("1", "2", "1", "2", "1", "2", "3");
+        list.stream().forEach(
+                v ->{
+                    Object o = map.get(v);
+                    if(map.get(v) == null){
+                        map.put(v, 1);
+                    } else {
+                        map.put(v, Integer.valueOf(o.toString()) + 1);
+                    }
+                }
+        );
+
+       Iterator iter = map.entrySet().iterator();
+       while (iter.hasNext()){
+           Map.Entry next = (Map.Entry) iter.next();
+           String key = (String)next.getKey();
+           Integer val = (Integer) next.getValue();
+           System.out.println(key + ":" + val/2);
+       }
+    }
 }
