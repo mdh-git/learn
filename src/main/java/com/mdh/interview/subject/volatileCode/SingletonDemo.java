bf5cc591faa8f139012ff05797ba8ec737cc46c7
madonghao <madonghao>
2020-08-27 15:16:33 +0800

    fix： 在单例模式中使用volatile的补充

--- src/main/java/com/mdh/interview/subject/volatileCode/SingletonDemo.java
+++ src/main/java/com/mdh/interview/subject/volatileCode/SingletonDemo.java
@@ -20,6 +20,24 @@ public class SingletonDemo {
         return singleton;
     }
 
+    /**
+     * instance = new SingletonDemo();
+     *
+     *  可以分解为以下三个步骤
+     * 1 memory=allocate(); 分配内存 相当于c的malloc
+     * 2 ctorInstanc(memory) 初始化对象
+     * 3 s=memory 设置s指向刚分配的地址
+     *
+     *  上述三个步骤可能会被重排序为 1-3-2，也就是：
+     * 1 memory=allocate(); 分配内存 相当于c的malloc
+     * 3 s=memory 设置s指向刚分配的地址
+     * 2 ctorInstanc(memory) 初始化对象
+     *
+     * 而一旦假设发生了这样的重排序，比如线程A在第16行执行了步骤1和步骤3，但是步骤2还没有执行完。这个时候线程A执行到了第13行，它会判定instance不为空，然后直接返回了一个未初始化完成的instance！
+     *
+     * 详细的解释文档： https://www.imooc.com/article/276841
+     * @param args
+     */
     public static void main(String[] args) {
 //        System.out.println(SingletonDemo.getInstance() == SingletonDemo.getInstance());
 //        System.out.println(SingletonDemo.getInstance() == SingletonDemo.getInstance());
