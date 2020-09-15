80d2ebcc2de448d18eb71219d24e4bb0507b3658
madonghao <MAdh@0825>
2020-09-15 23:20:46 +0800

    feat: Semaphore的用法

---
+++ src/main/java/com/mdh/interview/subject/semaphore/SemaphoreDemo.java
@@ -0,0 +1,13 @@
+package com.mdh.interview.subject.semaphore;
+
+import java.util.concurrent.Semaphore;
+
+/**
+ * @author MDH
+ * 2020/9/15 23:12
+ */
+public class SemaphoreDemo {
+    public static void main(String[] args) {
+        Semaphore semaphore = new Semaphore(10);
+    }
+}
