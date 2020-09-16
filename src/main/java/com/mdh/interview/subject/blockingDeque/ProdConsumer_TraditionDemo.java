7755d7fb17cf007695f0697125841068c8dc51b1
madonghao <MAdh@0825>
2020-09-16 23:41:29 +0800

    feat: 生产者消费者传统版

---
+++ src/main/java/com/mdh/interview/subject/blockingDeque/ProdConsumer_TraditionDemo.java
@@ -0,0 +1,90 @@
+package com.mdh.interview.subject.blockingDeque;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * @author MDH
+ * 2020/9/16 23:15
+ *
+ * 生产者消费者模式传统版
+ *
+ * 题目: 一个初始化为零的变量,两个线程对其交替操作,一个加1一个减1
+ *
+ * 1. 线程  操作(方法)  资源类
+ * 2. 判断  干活  通知
+ * 3. 防止虚假唤醒机制(不能用if 使用while)
+ */
+public class ProdConsumer_TraditionDemo {
+    public static void main(String[] args) {
+        ShareData shareData = new ShareData();
+
+        new Thread(() -> {
+            for (int i = 1; i <= 5; i++) {
+                try {
+                    shareData.increment();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }, "AA").start();
+
+        new Thread(() -> {
+            for (int i = 1; i <= 5; i++) {
+                try {
+                    shareData.decrement();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }, "BB").start();
+    }
+}
+
+class ShareData {
+
+    private int number = 0;
+
+    private Lock lock = new ReentrantLock();
+    private Condition condition = lock.newCondition();
+
+    public void increment() throws Exception {
+        try {
+            lock.lock();
+            // 1 判断
+            while (number != 0){
+                // 等待,不能生产
+                condition.await();
+            }
+            // 2 干活
+            number++;
+            System.out.println(Thread.currentThread().getName() + "\t" + number);
+            // 3 通知唤醒
+            condition.signalAll();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void decrement() throws Exception {
+        try {
+            lock.lock();
+            while (number == 0){
+                // 等待,不能消费
+                condition.await();
+            }
+            // 2 干活
+            number--;
+            System.out.println(Thread.currentThread().getName() + "\t" + number);
+            // 3 通知唤醒
+            condition.signalAll();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+    }
+}
