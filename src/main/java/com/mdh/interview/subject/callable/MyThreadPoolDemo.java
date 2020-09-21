96c203e1d35d647b28be130cafd62acdcb5034ee
madonghao <MAdh@0825>
2020-09-22 00:37:15 +0800

    feat: 线程池的底层原理以及线程池配置分析

--- src/main/java/com/mdh/interview/subject/callable/MyThreadPoolDemo.java
+++ src/main/java/com/mdh/interview/subject/callable/MyThreadPoolDemo.java
@@ -1,8 +1,8 @@
 package com.mdh.interview.subject.callable;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
+
+import java.util.concurrent.*;
 
 /**
  * @author MDH
@@ -17,12 +17,43 @@ public class MyThreadPoolDemo {
 
         // single();
 
-        cached();
+        // cached();
 
+
+        //初始化线程
+        ThreadFactory namedThreadFactory = new ThreadFactoryBuilder().setNameFormat("thread-call-runner-%d").build();
+
+        BlockingQueue<Runnable> workQueue = new LinkedBlockingQueue<>(3);
+        ThreadPoolExecutor.AbortPolicy abortPolicy = new ThreadPoolExecutor.AbortPolicy();
+        ThreadPoolExecutor.CallerRunsPolicy callerRunsPolicy = new ThreadPoolExecutor.CallerRunsPolicy();
+        ThreadPoolExecutor.DiscardOldestPolicy discardOldestPolicy = new ThreadPoolExecutor.DiscardOldestPolicy();
+        ThreadPoolExecutor.DiscardPolicy discardPolicy = new ThreadPoolExecutor.DiscardPolicy();
+        ExecutorService threadPool = new ThreadPoolExecutor(
+                2,
+                5,
+                1L,
+                TimeUnit.SECONDS,
+                workQueue,
+                Executors.defaultThreadFactory(),
+                discardOldestPolicy
+        );
+
+        // i分别尝试 1  3  5  8  9看结果
+        try {
+            for (int i = 0; i < 10; i++) {
+                threadPool.execute(() -> {
+                    System.out.println(Thread.currentThread().getName() + "  处理");
+                });
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            threadPool.shutdown();
+        }
     }
 
     /**
-     * 一池多个处理线程
+     * 一池多个处理线程 可缓存的
      */
     private static void cached() {
         // 一池多个处理线程
