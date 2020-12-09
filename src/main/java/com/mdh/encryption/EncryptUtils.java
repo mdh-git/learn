c9bd1d6c82d99e20c21109cf7353528c44f91618
madonghao <madonghao@imdada.cn>
2020-12-09 16:51:09 +0800

    feat: 补充

--- src/main/java/com/mdh/encryption/EncryptUtils.java
+++ src/main/java/com/mdh/encryption/EncryptUtils.java
@@ -7,6 +7,9 @@ import sun.misc.BASE64Decoder;
 import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
 import javax.crypto.spec.SecretKeySpec;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @Author: madonghao
  * @Date: 2020/12/9 4:00 下午
@@ -112,5 +115,59 @@ public class EncryptUtils {
      */
     private static String aesDecrypt(String encryptStr, String decryptKey) throws Exception {
         return StringUtils.isEmpty(encryptStr) ? null : aesDecryptByBytes(base64Decode(encryptStr), decryptKey);
+    }
+
+
+    /**
+     * 把原始字符串分割成指定长度的字符串列表
+     *
+     * @param inputString 原始字符串
+     * @param length 指定长度
+     * @return
+     */
+    public static List<String> getStrList(String inputString, int length) {
+        int size = inputString.length() / length;
+        if (inputString.length() % length != 0) {
+            size += 1;
+        }
+        return getStrList(inputString, length, size);
+    }
+
+    /**
+     * 把原始字符串分割成指定长度的字符串列表
+     *
+     * @param inputString 原始字符串
+     * @param length 指定长度
+     * @param size 指定列表大小
+     * @return
+     */
+    public static List<String> getStrList(String inputString, int length,
+                                          int size) {
+        List<String> list = new ArrayList<>();
+        for (int index = 0; index < size; index++) {
+            String childStr = substring(inputString, index * length,
+                    (index + 1) * length);
+            list.add(childStr);
+        }
+        return list;
+    }
+
+    /**
+     * 分割字符串，如果开始位置大于字符串长度，返回空
+     *
+     * @param str 原始字符串
+     * @param f 开始位置
+     * @param t 结束位置
+     * @return
+     */
+    public static String substring(String str, int f, int t) {
+        if (f > str.length()) {
+            return null;
+        }
+        if (t > str.length()) {
+            return str.substring(f, str.length());
+        } else {
+            return str.substring(f, t);
+        }
     }
 }
